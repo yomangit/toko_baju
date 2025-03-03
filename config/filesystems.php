a8304e90b05591b52f38316cadd64a506877296a
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-03 09:28:35 +0800

    refactor: update public filesystem root path for consistency

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             // 'root' => storage_path('app/public'),
-            'root'   => public_path() . '/myfiles',
+            'root'   => public_path('public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
