b3977601d0ef4657fb2440c4ce66368d46b81766
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-03 09:41:21 +0800

    debug: update photo variable dump in store method for better path visibility

--- app/Livewire/Administrator/Stok/Create.php
+++ app/Livewire/Administrator/Stok/Create.php
@@ -75,7 +75,7 @@ class Create extends ModalComponent
     public function store()
     {
 
-        dd($this->photo->getClientOriginalName());
+        dd($this->photo->getPathName());
         $this->validate();
         if ($this->photo) {
             $this->nama_foto = $this->photo->getClientOriginalName();
