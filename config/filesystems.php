92db2d3c5e9f2af7d2e56affb11857274e3f01bb
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-03 09:31:31 +0800

    refactor: update public filesystem root path to point to myfiles directory

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             // 'root' => storage_path('app/public'),
-            'root'   => public_path('public'),
+            'root'   => public_path() . '/myfiles',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
