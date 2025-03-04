8c0202f8e430427d197693b668f5970f65ca5bb6
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 13:47:26 +0800

    fix: adjust input padding and SVG icon size for improved layout in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -5,9 +5,10 @@
             <legend class="fieldset-legend">Page details</legend>
             <div class="relative flex items-center">
                 <input id="11" type="text"
-                    class="relative block pl-4 pr-4 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1" />
+                    class="relative block pl-8 pr-4 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1" />
 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="absolute size-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                    class="absolute w-4 size-4">
                     <path fill-rule="evenodd"
                         d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
                         clip-rule="evenodd" />
