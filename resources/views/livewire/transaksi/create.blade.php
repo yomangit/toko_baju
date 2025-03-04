e4be3fe57e374f5bca1c79f7923a180b973ab579
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 14:41:47 +0800

    fix: simplify layout by removing unnecessary flex properties in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -44,7 +44,7 @@
                     </div>
                     <x-input-error :messages="$errors->get('harga_jual')" />
                 </fieldset>
-                <div class="flex flex-row items-end">
+                <div class="self-end">
                     Total : Rp {{ $total }}
                 </div>
             </div>
