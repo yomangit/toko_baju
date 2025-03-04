3a45aeac3f88495d56226dbf9d4dd5ec7a3d9aec
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 13:28:58 +0800

    fix: adjust SVG positioning and add padding to text input in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -4,14 +4,14 @@
         <fieldset class="p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
             <legend class="fieldset-legend">Page details</legend>
             <label class="relative ">
-                <span class="absolute inset-y-0 left-0 z-20"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
-                        fill="currentColor" class="size-4">
+                <span class="absolute inset-y-0 left-0 z-20 -top-1"><svg xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16" fill="currentColor" class="size-4">
                         <path fill-rule="evenodd"
                             d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
                             clip-rule="evenodd" />
                     </svg>
                 </span>
-                <x-text-input wire:model.live='nama_pakaian' :error="$errors->get('nama_pakaian')" type="text"
+                <x-text-input wire:model.live='nama_pakaian' class="pl-4" :error="$errors->get('nama_pakaian')" type="text"
                     placeholder="Nama Pakaian" />
             </label>
             <fieldset class="pb-0.5 fieldset ">
