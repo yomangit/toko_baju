bedd2e59b681667fb7c00b5032040a6440678ddd
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 11:50:55 +0800

    feat: add fieldset for page details in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -1,7 +1,18 @@
 <div>
 
     <div class="flex justify-between ...">
-        <div class="bg-sky-500">01</div>
+        <fieldset class="p-4 border fieldset w-xs bg-base-200 border-base-300 rounded-box">
+            <legend class="fieldset-legend">Page details</legend>
+
+            <label class="fieldset-label">Title</label>
+            <input type="text" class="input" placeholder="My awesome page" />
+
+            <label class="fieldset-label">Slug</label>
+            <input type="text" class="input" placeholder="my-awesome-page" />
+
+            <label class="fieldset-label">Author</label>
+            <input type="text" class="input" placeholder="Name" />
+        </fieldset>
         <div class="bg-orange-500">02</div>
 
     </div>
