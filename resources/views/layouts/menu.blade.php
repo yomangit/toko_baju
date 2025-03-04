108ce2a36ae12d4fd831e1cb8b7f37ea4e64c3b8
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 11:38:49 +0800

    feat: add 'Transaksi' menu item to the admin layout

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,6 +9,18 @@
     </a>
 </li>
 <li>
+    <a class="{{ Request::is('admin/transaksi/new') ? 'menu-active' : '' }}" href="{{ route('transaksi.new') }}">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+            <path fill-rule="evenodd"
+                d="M10.47 2.22a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 1 1-1.06-1.06l.97-.97H5.75a.75.75 0 0 1 0-1.5h5.69l-.97-.97a.75.75 0 0 1 0-1.06Zm-4.94 6a.75.75 0 0 1 0 1.06l-.97.97h5.69a.75.75 0 0 1 0 1.5H4.56l.97.97a.75.75 0 1 1-1.06 1.06l-2.25-2.25a.75.75 0 0 1 0-1.06l2.25-2.25a.75.75 0 0 1 1.06 0Z"
+                clip-rule="evenodd" />
+        </svg>
+
+
+        Transaksi
+    </a>
+</li>
+<li>
     <a class="{{ Request::is('admin/stok') ? 'menu-active' : '' }}" href="{{ route('stok') }}">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
             <path fill-rule="evenodd"
@@ -20,7 +32,8 @@
     </a>
 </li>
 <li>
-    <a class="{{ Request::is('admin/CustomerController') ? 'menu-active' : '' }}" href="{{ route('CustomerController') }}">
+    <a class="{{ Request::is('admin/CustomerController') ? 'menu-active' : '' }}"
+        href="{{ route('CustomerController') }}">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
             <path
                 d="M8 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM3.156 11.763c.16-.629.44-1.21.813-1.72a2.5 2.5 0 0 0-2.725 1.377c-.136.287.102.58.418.58h1.449c.01-.077.025-.156.045-.237ZM12.847 11.763c.02.08.036.16.046.237h1.446c.316 0 .554-.293.417-.579a2.5 2.5 0 0 0-2.722-1.378c.374.51.653 1.09.813 1.72ZM14 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM3.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 13c-.552 0-1.013-.455-.876-.99a4.002 4.002 0 0 1 7.753 0c.136.535-.324.99-.877.99H5Z" />
