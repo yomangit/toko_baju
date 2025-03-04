c605ac6063d73bdd62758e870b6eb7e65cf63485
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 10:43:48 +0800

    fix: improve input focus styling in inputwithlabel component for better user experience

--- resources/views/components/inputwithlable.blade.php
+++ resources/views/components/inputwithlable.blade.php
@@ -24,5 +24,6 @@
                 clip-rule="evenodd" />
         </svg>
     </span>
-    <input class="border" type="text" placeholder="URL" />
+    <input class="border focus:outline-none focus:border-accent focus:ring-accent focus:ring-1" type="text"
+        placeholder="URL" />
 </label>
