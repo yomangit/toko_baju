0c010a4e0d9ce4a50c7084358c00021691ee331b
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 14:31:01 +0800

    fix: update layout for quantity input to improve alignment in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -15,7 +15,7 @@
                 <x-text-input wire:model.live='harga_jual' :error="$errors->get('harga_jual')" type="number" placeholder="Harga Jual" />
                 <x-input-error :messages="$errors->get('harga_jual')" />
             </fieldset>
-            <div class="flex justify-between">
+            <div class="flex items-center justify-between">
                 <fieldset class="pb-0.5 fieldset ">
                     <x-lable-req>{{ __('Jumlah') }}</x-lable-req>
                     <div class="join">
