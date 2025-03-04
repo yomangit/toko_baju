c80a33a59e1afeb957d88280bfeb755205097227
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 13:32:37 +0800

    fix: update input structure and styling for improved search functionality in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -3,16 +3,16 @@
     <div class="flex justify-between ...">
         <fieldset class="p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
             <legend class="fieldset-legend">Page details</legend>
-            <label class="relative p-0">
-                <span class="absolute inset-y-0 left-0 z-20 -top-1"><svg xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16" fill="currentColor" class="size-4">
-                        <path fill-rule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </span>
-                <x-text-input wire:model.live='nama_pakaian' class="pl-4" :error="$errors->get('nama_pakaian')" type="text"
-                    placeholder="Nama Pakaian" />
+            <label class="input">
+                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                        stroke="currentColor">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input type="search" class="grow" placeholder="Search" />
+
             </label>
             <fieldset class="pb-0.5 fieldset ">
                 <x-lable-req>{{ __('nama pakaian') }}</x-lable-req>
