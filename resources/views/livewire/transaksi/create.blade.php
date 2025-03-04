aebd89854fcc6d016efcdb071c634c1e251c26e6
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 11:55:01 +0800

    feat: enhance Transaksi create component with new input fields and improved styling

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -1,11 +1,14 @@
 <div>
 
     <div class="flex justify-between ...">
-        <fieldset class="p-4 border fieldset w-xs bg-base-200 border-base-300 rounded-box">
+        <fieldset class="p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
             <legend class="fieldset-legend">Page details</legend>
-
-            <label class="fieldset-label">Title</label>
-            <input type="text" class="input" placeholder="My awesome page" />
+            <x-text-cari wire:model.live='Pencarian' type="text" placeholder="Pencarian" />
+            <fieldset class="pb-0.5 fieldset ">
+                <x-lable-req>{{ __('nama pakaian') }}</x-lable-req>
+                <x-text-input wire:model.live='nama_pakaian' :error="$errors->get('nama_pakaian')" type="text" placeholder="Nama Pakaian" />
+                <x-input-error :messages="$errors->get('nama_pakaian')" />
+            </fieldset>
 
             <label class="fieldset-label">Slug</label>
             <input type="text" class="input" placeholder="my-awesome-page" />
