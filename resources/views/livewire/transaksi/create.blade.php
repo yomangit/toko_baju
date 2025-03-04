d4d2b1d8f5f4d6ab097eb19a854d812686f258a9
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-04 13:26:46 +0800

    fix: update label structure for improved positioning in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -3,9 +3,9 @@
     <div class="flex justify-between ...">
         <fieldset class="p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
             <legend class="fieldset-legend">Page details</legend>
-            <label class="input input-xs focus:outline-none outline-offset-0 focus:outline-hidden">
-                <span class="label"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                        class="size-4">
+            <label class="relative ">
+                <span class="absolute inset-y-0 left-0"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"
+                        fill="currentColor" class="size-4">
                         <path fill-rule="evenodd"
                             d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
                             clip-rule="evenodd" />
