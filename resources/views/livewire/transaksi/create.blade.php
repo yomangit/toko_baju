87935bb2232ab4bf5eadad62bcae5f9b1d63b303
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-05 09:57:05 +0800

    fix: update input binding for count field and modify label for total field in Transaksi create component; add page details section with sample data

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -30,7 +30,7 @@
                                 </svg>
 
                             </label>
-                            <input type="text" wire:model='count'
+                            <input type="text" wire:model.live='count'
                                 class="w-16 input input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1">
                             <label wire:click="increment" class="join-item btn btn-xs btn-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
@@ -46,7 +46,7 @@
                     <x-input-error :messages="$errors->get('harga_jual')" />
                 </fieldset>
                 <fieldset class=" fieldset">
-                    <x-lable-req>{{ __('Total') }}</x-lable-req>
+                    <x-lable>{{ __('Total') }}</x-lable>
                     <div class=" join">
                         <span class="rounded-l-full btn btn-xs join-item btn-ghost bg-transparent"><svg
                                 fill="currentColor" class="size-4" viewBox="0 0 24 24" id="rupiah-2"
@@ -72,7 +72,46 @@
 
             </div>
         </fieldset>
-        <div class="bg-orange-500">02</div>
+        <div class="bg-orange-500">
+            <fieldset class="p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
+                <legend class="fieldset-legend">Page details</legend>
+                <div class="overflow-x-auto">
+                    <table class="table table-xs">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>company</th>
+                                <th>location</th>
+                                <th>Last Login</th>
+                                <th>Favorite Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Littel, Schaden and Vandervort</td>
+                                <td>Canada</td>
+                                <td>12/16/2020</td>
+                                <td>Blue</td>
+                            </tr>
+                            <tr>
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Desktop Support Technician</td>
+                                <td>Zemlak, Daniel and Leannon</td>
+                                <td>United States</td>
+                                <td>12/5/2020</td>
+                                <td>Purple</td>
+                            </tr>
+
+                    </table>
+                </div>
+            </fieldset>
+        </div>
 
     </div>
 </div>
