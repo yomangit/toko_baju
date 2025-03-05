7e655867eb8c9c7914934cbe7bf544f29e9832b9
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-05 08:59:25 +0800

    fix: remove redundant class from join item button in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -44,7 +44,7 @@
                             </label>
                         </div>
                         <div class=" join">
-                            <span class="rounded-l-full btn join-item"><svg fill="currentColor" class="size-4"
+                            <span class="rounded-l-full join-item"><svg fill="currentColor" class="size-4"
                                     viewBox="0 0 24 24" id="rupiah-2" data-name="Flat Line"
                                     xmlns="http://www.w3.org/2000/svg" class="icon flat-line">
                                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
