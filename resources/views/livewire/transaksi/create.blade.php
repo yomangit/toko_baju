0d49171b9c72f13384ef1ad40f66f9e40fbc2c3d
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-06 13:14:34 +0800

    fix: update input binding for count field in Transaksi create component to use live model

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -29,7 +29,7 @@
                                         clip-rule="evenodd" />
                                 </svg>
                             </label>
-                            <input type="text" wire:model='count'
+                            <input type="text" wire:model.live='count'
                                 class="w-16 input input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1">
                             <label wire:click="increment" class="join-item btn btn-xs btn-secondary">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
