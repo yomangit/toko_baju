51e7d05500f58bd026e08fd9f9cf2864d6248d35
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 07:55:54 +0800

    fix: update wire:model binding in Transaksi create component for improved data synchronization

--- resources/views/livewire/administrator/stok/create.blade.php
+++ resources/views/livewire/administrator/stok/create.blade.php
@@ -46,8 +46,8 @@
                     @foreach ($UkuranPakaian as $index => $size)
                         <fieldset class="pb-0.5 fieldset ">
                             <x-lable-req>{{ __('ukuran ') }}{{ $size->ukuran_pakaian }}</x-lable-req>
-                            <x-text-size wire:ignore wire:model.live="filds.{{ $size->ukuran_pakaian }}"
-                                :error="$errors->get('filds.' . $size->ukuran_pakaian)" type="number" placeholder="Stok" />
+                            <x-text-size wire:ignore wire:model="filds.{{ $size->ukuran_pakaian }}" :error="$errors->get('filds.' . $size->ukuran_pakaian)"
+                                type="number" placeholder="Stok" />
                             <x-input-error :messages="$errors->get('filds.' . $size->ukuran_pakaian)" />
                         </fieldset>
                     @endforeach
