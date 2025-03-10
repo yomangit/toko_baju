b978d640cc0a6a611871ea132f2dc2905ed08059
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 10:46:29 +0800

    fix: optimize search handling in Transaksi create component to prevent unnecessary queries

--- app/Livewire/Transaksi/Create.php
+++ app/Livewire/Transaksi/Create.php
@@ -21,17 +21,19 @@ class Create extends Component
     }
     public function render()
     {
-        if ($this->search) {
-            $stok = StokPakaian::search(trim($this->search))->first();
-            $this->harga_satuan = $stok->harga_jual;
-            $this->nama_pakaian = $stok->nama_pakaian;
-            $this->stok = $stok->jumlah_stok;
-            $this->total = $this->count * $this->harga_satuan;
-        } else {
-            $this->harga_satuan = 0;
-            $this->nama_pakaian = '';
-            $this->stok = 0;
-            $this->total = 0;
+        if (StokPakaian::search(trim($this->search))->exists()) {
+            if ($this->search) {
+                $stok = StokPakaian::search(trim($this->search))->first();
+                $this->harga_satuan = $stok->harga_jual;
+                $this->nama_pakaian = $stok->nama_pakaian;
+                $this->stok = $stok->jumlah_stok;
+                $this->total = $this->count * $this->harga_satuan;
+            } else {
+                $this->harga_satuan = 0;
+                $this->nama_pakaian = '';
+                $this->stok = 0;
+                $this->total = 0;
+            }
         }
         return view('livewire.transaksi.create')->extends('layouts.app', ['header' => 'Transaksi Baru', 'title' => 'Transaksi Baru'])->section('content');
     }
