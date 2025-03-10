e0c57874d565d3b6afa43f8fdc826f40e9e1dbc5
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 08:09:44 +0800

    fix: update harga_satuan and stok assignments in Transaksi create component for accurate data retrieval

--- app/Livewire/Transaksi/Create.php
+++ app/Livewire/Transaksi/Create.php
@@ -23,9 +23,9 @@ class Create extends Component
     {
         if ($this->search) {
             $stok = StokPakaian::search(trim($this->search))->first();
-            $this->harga_satuan = $stok->harga;
+            $this->harga_satuan = $stok->harga_jual;
             $this->nama_pakaian = $stok->nama_pakaian;
-            $this->stok = $stok->stok;
+            $this->stok = $stok->jumlah_stok;
             $this->total = $this->count * $this->harga_satuan;
         }
         return view('livewire.transaksi.create')->extends('layouts.app', ['header' => 'Transaksi Baru', 'title' => 'Transaksi Baru'])->section('content');
