aa038c7649b041870c594321da5e2eb38d91e89a
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 09:22:20 +0800

    feat: add page details section with title, slug, and author inputs in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -124,6 +124,18 @@
                         </tr>
                 </table>
             </div>
+            <fieldset class="p-4 border fieldset w-xs bg-base-200 border-base-300 rounded-box">
+                <legend class="fieldset-legend">Page details</legend>
+
+                <label class="fieldset-label">Title</label>
+                <input type="text" class="input" placeholder="My awesome page" />
+
+                <label class="fieldset-label">Slug</label>
+                <input type="text" class="input" placeholder="my-awesome-page" />
+
+                <label class="fieldset-label">Author</label>
+                <input type="text" class="input" placeholder="Name" />
+            </fieldset>
         </fieldset>
 
 
