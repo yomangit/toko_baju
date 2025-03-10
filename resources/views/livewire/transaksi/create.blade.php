fc9fcfd840aac164f073bcd45feb7484fbb38c8c
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 09:17:51 +0800

    feat: update Transaksi create component table structure and add delete functionality

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -97,13 +97,11 @@
                 <table class="table table-xs">
                     <thead>
                         <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>company</th>
-                            <th>location</th>
-                            <th>Last Login</th>
-                            <th>Favorite Color</th>
+                            <th>No.</th>
+                            <th>Name Pakaian</th>
+                            <th>Jumlah</th>
+                            <th>#</th>
+
                         </tr>
                     </thead>
                     <tbody>
@@ -111,19 +109,23 @@
                             <th>1</th>
                             <td>Cy Ganderton</td>
                             <td>Quality Control Specialist</td>
-                            <td>Littel, Schaden and Vandervort</td>
-                            <td>Canada</td>
-                            <td>12/16/2020</td>
-                            <td>Blue</td>
+                            <td>
+                                <x-icon-btn-delete data-tip="Hapus" data-tip="delete"
+                                    wire:click='destroy({{ $customer->id }})'
+                                    wire:confirm.prompt="Are you sure delete {{ $customer->name }}?\n\nType DELETE to confirm|DELETE" />
+                            </td>
+
                         </tr>
                         <tr>
                             <th>2</th>
                             <td>Hart Hagerty</td>
                             <td>Desktop Support Technician</td>
-                            <td>Zemlak, Daniel and Leannon</td>
-                            <td>United States</td>
-                            <td>12/5/2020</td>
-                            <td>Purple</td>
+                            <td>
+                                <x-icon-btn-delete data-tip="Hapus" data-tip="delete"
+                                    wire:click='destroy({{ $customer->id }})'
+                                    wire:confirm.prompt="Are you sure delete {{ $customer->name }}?\n\nType DELETE to confirm|DELETE" />
+                            </td>
+
                         </tr>
                 </table>
             </div>
