ceeeb940e844d52518b5a44b3bf7c6d7e9ede927
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 08:25:02 +0800

    fix: add card class to wrapper div for improved styling

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -1,7 +1,7 @@
 <div>
 
     <div class="flex gap-4">
-        <div>
+        <div class="card">
             <fieldset class="flex-none p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
                 <legend class="fieldset-legend">Page details</legend>
                 <fieldset class="pb-0.5 fieldset ">
