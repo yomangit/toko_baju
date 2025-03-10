1c37d36a3a46e75cda7e304e4d2bedeec08350f0
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 09:47:06 +0800

    fix: correct fieldset nesting in Transaksi create component for improved structure

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -126,18 +126,18 @@
                         </tr>
                 </table>
             </div>
-        </fieldset>
-        <fieldset class="p-4 border fieldset w-xs bg-base-200 border-base-300 rounded-box">
-            <legend class="fieldset-legend">Page details</legend>
+            <fieldset class="p-4 border fieldset w-xs bg-base-200 border-base-300 rounded-box">
+                <legend class="fieldset-legend">Page details</legend>
 
-            <label class="fieldset-label">Title</label>
-            <input type="text" class="input" placeholder="My awesome page" />
+                <label class="fieldset-label">Title</label>
+                <input type="text" class="input" placeholder="My awesome page" />
 
-            <label class="fieldset-label">Slug</label>
-            <input type="text" class="input" placeholder="my-awesome-page" />
+                <label class="fieldset-label">Slug</label>
+                <input type="text" class="input" placeholder="my-awesome-page" />
 
-            <label class="fieldset-label">Author</label>
-            <input type="text" class="input" placeholder="Name" />
+                <label class="fieldset-label">Author</label>
+                <input type="text" class="input" placeholder="Name" />
+            </fieldset>
         </fieldset>
     </div>
 </div>
