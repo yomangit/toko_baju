8d9b6c67169f068f7afb8796071db7ee2e5bf858
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 08:12:29 +0800

    fix: reset harga_satuan, nama_pakaian, stok, and total in Transaksi create component for proper initialization

--- app/Livewire/Transaksi/Create.php
+++ app/Livewire/Transaksi/Create.php
@@ -27,6 +27,11 @@ class Create extends Component
             $this->nama_pakaian = $stok->nama_pakaian;
             $this->stok = $stok->jumlah_stok;
             $this->total = $this->count * $this->harga_satuan;
+        } else {
+            $this->harga_satuan = 0;
+            $this->nama_pakaian = '';
+            $this->stok = 0;
+            $this->total = 0;
         }
         return view('livewire.transaksi.create')->extends('layouts.app', ['header' => 'Transaksi Baru', 'title' => 'Transaksi Baru'])->section('content');
     }
