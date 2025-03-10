c8379c5d5ffde4eb1146d4ee22d04d84ff8f566b
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 08:03:25 +0800

    fix: enhance search input layout with custom styling and icon in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -4,11 +4,21 @@
         <fieldset class="flex-none p-4 border shadow-md fieldset w-xs bg-base-200 border-base-300 rounded-box">
             <legend class="fieldset-legend">Page details</legend>
             <fieldset class="pb-0.5 fieldset ">
-                <x-inputwithlable wire:model.live='search' type="text"placeholder="cari kode pakaian" />
+                <div class="relative flex items-center">
+                    <input id="11"
+                        class="'relative block pl-8 pr-4 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1'" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                        class="absolute ml-3 size-4">
+                        <path fill-rule="evenodd"
+                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </div>
             </fieldset>
             <fieldset class="pb-0.5 fieldset ">
                 <x-lable-req>{{ __('nama pakaian') }} </x-lable-req>
-                <x-text-input wire:model.live='nama_pakaian' :error="$errors->get('nama_pakaian')" type="text" placeholder="Nama Pakaian" />
+                <x-text-input wire:model.live='nama_pakaian' :error="$errors->get('nama_pakaian')" type="text"
+                    placeholder="Nama Pakaian" />
                 <x-input-error :messages="$errors->get('nama_pakaian')" />
             </fieldset>
             <fieldset class="pb-0.5 fieldset ">
