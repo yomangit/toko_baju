bfb7369c5f631974c6ab926521981f6a4331dc82
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 08:50:10 +0800

    fix: combine fieldset and card for improved layout in Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -90,45 +90,45 @@
 
             </div>
         </fieldset>
-        <div class="card grow">
-            <fieldset class="fieldset rounded-box">
-                <legend class="fieldset-legend">Keranjang</legend>
-                <div class="m-4 overflow-x-auto shadow-md card-body bg-base-200 rounded-box">
-                    <table class="table table-xs">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Name</th>
-                                <th>Job</th>
-                                <th>company</th>
-                                <th>location</th>
-                                <th>Last Login</th>
-                                <th>Favorite Color</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>Cy Ganderton</td>
-                                <td>Quality Control Specialist</td>
-                                <td>Littel, Schaden and Vandervort</td>
-                                <td>Canada</td>
-                                <td>12/16/2020</td>
-                                <td>Blue</td>
-                            </tr>
-                            <tr>
-                                <th>2</th>
-                                <td>Hart Hagerty</td>
-                                <td>Desktop Support Technician</td>
-                                <td>Zemlak, Daniel and Leannon</td>
-                                <td>United States</td>
-                                <td>12/5/2020</td>
-                                <td>Purple</td>
-                            </tr>
-                    </table>
-                </div>
-            </fieldset>
-        </div>
+
+        <fieldset class="fieldset rounded-box card grow">
+            <legend class="fieldset-legend">Keranjang</legend>
+            <div class="m-4 overflow-x-auto shadow-md card-body bg-base-200 rounded-box">
+                <table class="table table-xs">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>company</th>
+                            <th>location</th>
+                            <th>Last Login</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Littel, Schaden and Vandervort</td>
+                            <td>Canada</td>
+                            <td>12/16/2020</td>
+                            <td>Blue</td>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Zemlak, Daniel and Leannon</td>
+                            <td>United States</td>
+                            <td>12/5/2020</td>
+                            <td>Purple</td>
+                        </tr>
+                </table>
+            </div>
+        </fieldset>
+
 
     </div>
 </div>
