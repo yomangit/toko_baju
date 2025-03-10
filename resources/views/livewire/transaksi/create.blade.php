9ce85225df2986ddc3d42d7b55ac54a037795c8b
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-10 08:08:27 +0800

    fix: update wire:model binding syntax in search input of Transaksi create component

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -5,7 +5,7 @@
             <legend class="fieldset-legend">Page details</legend>
             <fieldset class="pb-0.5 fieldset ">
                 <div class="relative flex items-center">
-                    <input id="11" wire:model.ive='search' type="text" placeholder="cari kode pakaian"
+                    <input id="11" wire:model.live='search' type="text" placeholder="cari kode pakaian"
                         class="'relative block pl-8 pr-4 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1'" />
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
                         class="absolute ml-3 size-4">
