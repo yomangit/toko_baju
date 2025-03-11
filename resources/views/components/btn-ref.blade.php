c23f18fc4e351a650b5d76d5ee20d550074e6065
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-11 13:46:00 +0800

    fix: update btn-ref component to use new SVG and improve styling

--- resources/views/components/btn-ref.blade.php
+++ resources/views/components/btn-ref.blade.php
@@ -1,50 +1,12 @@
-<a {{ $slot }} {{ $attributes->merge(['class' => 'btn btn-dash btn-accent btn-xs']) }}>
+<a {{ $slot }}
+    {{ $attributes->merge(['class' => 'btn btn-square btn-xs btn-primary tooltip tooltip-primary']) }}>
 
     <?xml version="1.0" encoding="utf-8"?>
 
     <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-    <svg version="1.1" id="Uploaded to svgrepo.com" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink" class="size-5 ml-[5%] mt-[5%]" viewBox="0 0 32 32" xml:space="preserve">
-        <style type="text/css">
-            .pictogram_zes {
-                fill: #0C6667;
-            }
-
-            .pictogram_vijf {
-                fill: #01A59C;
-            }
-
-            .pictogram_twee {
-                fill: #F8AD89;
-            }
-
-            .pictogram_een {
-                fill: #F4D6B0;
-            }
-
-            .st0 {
-                fill: none;
-            }
-
-            .st1 {
-                fill: #E54D2E;
-            }
-
-            .st2 {
-                fill: #F27261;
-            }
-        </style>
-        <g>
-            <circle class="pictogram_een" cx="16" cy="16" r="16" />
-            <path class="pictogram_twee" d="M4,16c0,6.627,5.373,12,12,12V4C9.373,4,4,9.373,4,16z" />
-            <path class="pictogram_vijf"
-                d="M16,0v4c6.627,0,12,5.373,12,12c0,6.627-5.373,12-12,12v4c8.837,0,16-7.163,16-16S24.837,0,16,0z" />
-            <path class="pictogram_vijf"
-                d="M16,0v4c6.627,0,12,5.373,12,12c0,6.627-5.373,12-12,12v4c8.837,0,16-7.163,16-16S24.837,0,16,0z" />
-            <path class="pictogram_zes" d="M16,28v4C7.163,32,0,24.837,0,16S7.163,0,16,0v4C9.373,4,4,9.373,4,16C4,22.627,9.373,28,16,28z
-             M22,14h-4.5V9.5C17.5,8.672,16.828,8,16,8s-1.5,0.672-1.5,1.5V14H10c-0.828,0-1.5,0.672-1.5,1.5S9.172,17,10,17h4.5v4.5
-            c0,0.828,0.672,1.5,1.5,1.5s1.5-0.672,1.5-1.5V17H22c0.828,0,1.5-0.672,1.5-1.5S22.828,14,22,14z" />
-        </g>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+        <path
+            d="M1.75 1.002a.75.75 0 1 0 0 1.5h1.835l1.24 5.113A3.752 3.752 0 0 0 2 11.25c0 .414.336.75.75.75h10.5a.75.75 0 0 0 0-1.5H3.628A2.25 2.25 0 0 1 5.75 9h6.5a.75.75 0 0 0 .73-.578l.846-3.595a.75.75 0 0 0-.578-.906 44.118 44.118 0 0 0-7.996-.91l-.348-1.436a.75.75 0 0 0-.73-.573H1.75ZM5 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM13 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" />
     </svg>
 
 </a>
