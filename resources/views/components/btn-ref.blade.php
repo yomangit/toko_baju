bf118cf43f0a27743f1105a001dbc90e7fb080a4
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-11 14:08:59 +0800

    fix: update btn-ref component SVG for improved design and size

--- resources/views/components/btn-ref.blade.php
+++ resources/views/components/btn-ref.blade.php
@@ -4,9 +4,10 @@
     <?xml version="1.0" encoding="utf-8"?>
 
     <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
-        <path
-            d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"class="size-5  ml-[1px] mt-[1.5px]">
+        <path fill-rule="evenodd"
+            d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z"
+            clip-rule="evenodd" />
     </svg>
 
 
