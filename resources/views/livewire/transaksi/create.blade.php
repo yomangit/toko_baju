03e869eedcf791d635ce5343df34c746c40ae96f
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-14 07:45:13 +0800

    input

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -189,7 +189,7 @@
                 sisa = split[0].length % 3,
                 rupiah = split[0].substr(0, sisa),
                 ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
+            console.log(number_string);
             if (ribuan) {
                 separator = sisa ? '.' : '';
                 rupiah += separator + ribuan.join('.');
@@ -197,7 +197,7 @@
 
             rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
             return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-            console.log(rupiah);
+
 
 
         }
