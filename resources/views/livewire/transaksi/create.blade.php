9af36e1432cc5cdbea2c012d2b837dae77446b96
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-14 07:40:13 +0800

    fix: remove console log from Rupiah formatting function

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -197,6 +197,7 @@
 
             rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
             return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+            console.log(rupiah);
 
 
         }
