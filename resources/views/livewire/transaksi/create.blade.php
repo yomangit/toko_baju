5543af02517fa2754d446b0eda91b555f591cff5
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-14 09:14:57 +0800

    dsdsd

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -95,8 +95,8 @@
             </fieldset>
         </div>
 
-        <div class="col-span-2 border shadow-md rounded-box card grow bg-base-200 border-base-300">
-            <fieldset class="p-4 fieldset ">
+        <div class="col-span-2">
+            <fieldset class="p-4 border shadow-md fieldset rounded-box card grow bg-base-200 border-base-300">
                 <legend class="fieldset-legend">Keranjang</legend>
                 <div
                     class="overflow-x-auto overflow-y-auto border rounded-sm  h-[40vh] rounded-box border-base-content/5">
