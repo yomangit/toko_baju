5dae015472c5e79d71117737313f5ad6bae4bc7a
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-18 15:05:19 +0800

    sdsd

--- resources/views/livewire/transaksi/detail.blade.php
+++ resources/views/livewire/transaksi/detail.blade.php
@@ -1,6 +1,6 @@
 <div>
     <x-notif />
-    <div class="flex items-center justify-end">
+    <div class="flex items-center justify-end py-2">
         <label class=" floating-label">
             <x-text-cari wire:model.live='searching' type="text" placeholder="Pencarian" />
             <span>Pencarian</span>
