5c19535757c9d1adde43af43196a52ce50e4c07f
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-18 11:48:29 +0800

    ewew

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -11,11 +11,11 @@
                                 role="button" placeholder="cari kode pakaian"
                                 class="'relative block pl-8 pr-4 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1'" />
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                                class="absolute ml-3 size-4">
-                                <path fill-rule="evenodd"
-                                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                                    clip-rule="evenodd" />
+                                class="size-4">
+                                <path
+                                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
                             </svg>
+
                         </div>
                         <ul tabindex="0" class="p-2 shadow-sm menu dropdown-content bg-base-100 rounded-box z-1 w-52">
                             <fieldset class="h-40 overflow-y-auto fieldset">
