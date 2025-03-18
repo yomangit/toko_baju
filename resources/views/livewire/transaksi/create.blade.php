1f100a00ab235566608a9bff7b50cce25005d550
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-18 11:49:14 +0800

    fdd

--- resources/views/livewire/transaksi/create.blade.php
+++ resources/views/livewire/transaksi/create.blade.php
@@ -8,7 +8,7 @@
                     <div class="dropdown dropdown-center">
                         <div class="relative flex items-center">
                             <input id="11" wire:model.live='customer_name' type="text" tabindex="0"
-                                role="button" placeholder="cari kode pakaian"
+                                role="button" placeholder="nama customer"
                                 class="'relative block pl-8 pr-4 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1'" />
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
                                 class="size-4">
