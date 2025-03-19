ba4daaa172ac2beb879148b55b63cb0a4e4ea653
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-19 09:41:49 +0800

    asdf

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,8 +1,9 @@
-@props(['error', 'type', 'disabled' => false])
+@props(['error', 'id', 'type', 'disabled' => false])
 
 <input @disabled($disabled)
     {{ $attributes->class([
         'block px-3 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 focus:outline-none focus:border-accent focus:ring-accent focus:ring-1',
         'block px-3 font-semibold border shadow-sm input input-bordered input-xs placeholder-slate-400 outline-none border-rose-500 ring-rose-500 ring-1' => $error,
     ]) }}
-    @isset($type) type="{{ $type }}" @endif />
+    @isset($type) type="{{ $type }}" @endif
+    @isset($id) id="{{ $id }}" @endif />
