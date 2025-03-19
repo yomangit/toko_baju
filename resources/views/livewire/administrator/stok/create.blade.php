7c9caa4baa3d4ead659ad6a8dd1e1adb0b316114
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-19 09:53:38 +0800

    ds

--- resources/views/livewire/administrator/stok/create.blade.php
+++ resources/views/livewire/administrator/stok/create.blade.php
@@ -168,6 +168,7 @@
                             sisa = split[0].length % 3,
                             rupiah = split[0].substr(0, sisa),
                             ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+                        console.log(number_string);
 
                         @this.set('harga_pokok', number_string)
                         if (ribuan) {
