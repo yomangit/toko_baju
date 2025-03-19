25a62c639c2b9fb0721ba764f3539116105c6e40
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-19 08:01:14 +0800

    asf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html data-theme="valentine" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html data-theme="garden" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ (!empty($title)? $title:"My Pink") }}</title>
+    <title>{{ !empty($title) ? $title : 'My Pink' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
