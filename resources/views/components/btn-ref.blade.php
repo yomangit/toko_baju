067db44236743985cd0d7fd12fb35731b521cbbe
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-19 09:27:55 +0800

    dsd

--- resources/views/components/btn-ref.blade.php
+++ resources/views/components/btn-ref.blade.php
@@ -4,7 +4,7 @@
     <?xml version="1.0" encoding="utf-8"?>
 
     <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"class="size-5  ml-[1px] mt-[1.5px]">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"class="size-4  ml-[1px] mt-[1.5px]">
         <path fill-rule="evenodd"
             d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z"
             clip-rule="evenodd" />
